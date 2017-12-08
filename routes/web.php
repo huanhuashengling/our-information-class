b4453cdc984ceb43f9afe6c4ab5b0630a00a2c70
ywj <shengling_2005@163.com>
2017-12-08 10:17:56 +0800

    copy multi auth web route

--- routes/web.php
+++ routes/web.php
@@ -35,38 +35,6 @@
     // Route::get('getStudentsData', 'HomeController@getStudentsData');
     // Route::post('resetStudentPassword', 'HomeController@resetStudentPassword');
 
-
-// });
-
-// Route::group(['middleware' => 'auth', 'namespace' => 'Teacher', 'prefix' => 'teacher'], function() {
-//     Route::get('/', 'HomeController@index');
-//     Route::resource('class', 'SchoolClassController');
-//     Route::resource('lesson', 'LessonController');
-//     Route::post('uploadMDImage', 'LessonController@uploadMDImage');
-//     Route::get('ajaxSearchTopics', 'LessonController@ajaxSearchTopics');
-
-//     Route::resource('createComment', 'CommentController@store');
-//     Route::resource('updateComment', 'CommentController@update');
-//     Route::resource('getCommentByPostsId', 'CommentController@getByPostsId');
-
-//     Route::resource('updateRate', 'HomeController@updateRate');
-//     Route::resource('getPostRate', 'HomeController@getPostRate');
-
-//     Route::get('lessonLog', 'LessonLogController@listLessonLog');
-//     Route::post('createLessonLog', 'LessonLogController@store');
-//     Route::post('updateLessonLog', 'LessonLogController@update');
-
-//     Route::get('takeclass', 'HomeController@takeClass');
-
-//     Route::get('getLessonPostPerSchoolClass', 'HomeController@getLessonPostPerSchoolClass');
-
-// });
-
-
-// Route::group(['middleware' => 'auth', 'namespace' => 'Student', 'prefix' => 'student'], function() {
-//     Route::get('/', 'HomeController@index');
-//     Route::post('upload', 'HomeController@upload');
-//     Route::get('posts', 'PostController@index');
 // });
 
 // Route::group(['middleware' => 'auth', 'namespace' => 'Teacher', 'prefix' => 'teacher'], function() {
@@ -137,7 +105,6 @@ Route::group(['middleware' => 'auth.teacher', 'prefix' => 'teacher','namespace' 
     $router->get('takeclass', 'HomeController@takeclass');
     $router->resource('lesson', 'LessonController');
     $router->resource('lessonLog', 'LessonLogController@listLessonLog');
-
     $router->post('getLessonPostPerSclass', 'HomeController@getLessonPostPerSclass');
 
     Route::post('uploadMDImage', 'LessonController@uploadMDImage');
@@ -154,8 +121,8 @@ Route::group(['middleware' => 'auth.teacher', 'prefix' => 'teacher','namespace' 
 
     $router->get('reset', 'HomeController@getReset');
     $router->post('reset', 'HomeController@postReset');
-}
 
+});
 
 Route::group(['prefix' => 'student','namespace' => 'Student'],function ($router)
 {
