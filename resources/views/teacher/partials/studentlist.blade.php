10e96dc15c0bf0dd9c56915038bceb66b8f4312a
ywj <shengling_2005@163.com>
2018-04-17 22:00:44 +0800

    change student post list use thumb logic

--- resources/views/teacher/partials/studentlist.blade.php
+++ resources/views/teacher/partials/studentlist.blade.php
@@ -31,8 +31,8 @@
     -->
     <div class="col-md-2 col-sm-4 col-xs-6" style="padding-left: 5px; padding-right: 5px;">
         <div class="panel panel-default">
-            <div class="text-center"><img class="{{$postClass}}" height="90px" value="{{ $studentPostData['id'] }}" src="{{$img}}"></div>
-            <!--<div class="text-center"><img class="img-responsive {{$postClass}}" value="{{ $studentPostData['id'] }}" src="{{ getThumbnail($img, 200, 140, 'fit') }}" alt=""></div>-->
+            <!--<div class="text-center"><img class="{{$postClass}}" height="90px" value="{{ $studentPostData['id'] }}" src="{{$img}}"></div>-->
+            <div class="text-center"><img class="img-responsive {{$postClass}}" value="{{ $studentPostData['id'] }}" src="{{ getThumbnail($img, 200, 140, 'fit') }}" alt=""></div>
             <div class="text-center"><h3><b>{{ $py->getFirstchar($student->username) }}</b> <small>{{ $student->username }}<span class="text-right"> (优|评|20赞)</span></small></h3>  </div>
 
         </div>
