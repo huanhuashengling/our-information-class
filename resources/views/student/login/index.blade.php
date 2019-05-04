b9a54600408a71a62280042f4f0338db3151e58c
ywj <shengling_2005@163.com>
2019-05-04 23:15:14 +0800

    add example VR images

--- resources/views/student/login/index.blade.php
+++ resources/views/student/login/index.blade.php
@@ -16,33 +16,34 @@
     <!-- <audio id="click-sound" src="/images/panorama/304_Hero.mp3"></audio> -->
 
     <!-- Images. -->
-    <img id="p1" src="/images/panorama/dalitang.jpg">
+    <img id="p1" src="/images/panorama/lt1.png">
     <!-- <img id="city-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg"> -->
-    <img id="p2" src="/images/panorama/meishujiaoshi.jpg">
+    <img id="p2" src="/images/panorama/lt2.png">
     <!-- <img id="cubes-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg"> -->
-    <img id="p3" src="/images/panorama/yinyuejiaoshi.jpg">
+    <img id="p3" src="/images/panorama/lt3.png">
     <!-- <img id="sechelt-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg"> -->
-    <img id="p4" src="/images/panorama/information-room1.jpg">
-    <img id="p5" src="/images/panorama/menting.jpg">
-    <img id="p6" src="/images/panorama/kexue.jpg">
+    <img id="p4" src="/images/panorama/lt4.png">
+    <img id="p5" src="/images/panorama/lt5.png">
+    <!-- <img id="p5" src="/images/panorama/menting.jpg"> -->
+    <!-- <img id="p6" src="/images/panorama/kexue.jpg"> -->
   </a-assets>
 
     <a-sky id="image-360"></a-sky>
 
-    <a-box src="/images/panorama/wubingliujinyun.png" rotation="45 45 45" position="1.5 0.25 1.5">
+    <a-box src="/images/panorama/sl1.png" rotation="45 45 45" position="1.5 0.25 1.5">
         <!-- <a-animation attribute="position" begin="click" to="1 0.25 1" direction="alternate" dur="100"></a-animation> -->
         <a-animation attribute="rotation" begin="click" to="360 360 360" dur="2000"></a-animation>
         <!-- <a-animation attribute="position" begin="mouseleave" to="2 0.25 2" dur="100"></a-animation> -->
     </a-box>
-    <a-box src="/images/panorama/sijiaguoyuduo.png" rotation="45 45 45" position="1.5 0.25 -1.5">
+    <a-box src="/images/panorama/sl3.png" rotation="45 45 45" position="1.5 0.25 -1.5">
         <!-- <a-animation attribute="position" to="2 0.25 -2" direction="alternate" dur="2000" repeat="indefinite"></a-animation> -->
         <a-animation attribute="rotation" begin="click" to="360 360 360" direction="alternate" dur="2000"></a-animation>
     </a-box>
-    <a-box src="/images/panorama/siyitangruiyao.png" rotation="45 45 45" position="-1.5 0.25 -1.5">
+    <a-box src="/images/panorama/sl4.png" rotation="45 45 45" position="-1.5 0.25 -1.5">
         <!-- <a-animation attribute="position" to="-2 0.25 -2" direction="alternate" dur="2000" repeat="indefinite"></a-animation> -->
         <a-animation attribute="rotation" begin="click" to="360 360 360" direction="alternate" dur="2000"></a-animation>
     </a-box>
-    <a-box src="/images/panorama/wudingshenweitao.png" rotation="45 45 45" position="-1.5 0.25 1.5">
+    <a-box src="/images/panorama/sl5.png" rotation="45 45 45" position="-1.5 0.25 1.5">
         <!-- <a-animation attribute="position" to="-2 0.25 2" direction="alternate" dur="2000" repeat="indefinite"></a-animation> -->
         <a-animation attribute="rotation" begin="click" to="360 360 360" direction="alternate" dur="2000"></a-animation>
     </a-box>
@@ -131,13 +132,15 @@
 
 <script>
 $(document).ready(function() {
-    var pnum = Math.floor(Math.random()*6) + 1;
+    var pnum = Math.floor(Math.random()*5) + 1;
     var pr = ["0 215 -0.8", "0 -9 -0.5", "0 190 0", "0 135 0", "0 -140 -2", "0 -180 0"];
     // var pr2 = "0 -9 -0.5";
     // var pr3 = "0 190 0";
     // var pr4 = "0 135 0";
     // var pr5 = "0 -140 -2";
+    // $("#image-360").attr("src", "#p1")
     $("#image-360").attr("src", "#p"+pnum)
+    // $("#image-360").attr("rotation", pr[0])
     $("#image-360").attr("rotation", pr[pnum-1])
     // $("#image-360").attr("src", "#p6");
     // $("#image-360").attr("rotation", "0 -180 0");
