37054cbc87625297db252688fc14cb1d77cdb8be
ywj <shengling_2005@163.com>
2019-05-09 22:38:17 +0800

    change student home page

--- resources/views/student/login/index.blade.php
+++ resources/views/student/login/index.blade.php
@@ -10,34 +10,26 @@
         <div class="text-center col-md-4">
             <!-- <img data-magnify="gallery" data-src="big-1.jpg" src="small-1.jpg" id='classmate-post-show'> -->
 
-            <img src="/images/panorama/青铜鼎.jpg" data-src="/images/panorama/青铜鼎.jpg" data-magnify="gallery" style="height: 300px" class="img-responsive img-thumbnail hidden" id="item1">
+            <img src="/images/panorama/青铜鼎.jpg" data-src="/images/panorama/青铜鼎.jpg" data-magnify="gallery" style="height: 300px" class="img-responsive img-thumbnail item hidden" id="item1">
         </div>
         <div class="text-center col-md-4">
-             <img src="/images/panorama/陶罐.jpg" data-src="/images/panorama/陶罐.jpg" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail hidden" id="item2">
+             <img src="/images/panorama/陶罐.jpg" data-src="/images/panorama/陶罐.jpg" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail item hidden" id="item2">
         </div>
         <div class="text-center col-md-4">
-             <img src="/images/panorama/衣服.jpg" data-src="/images/panorama/衣服.jpg" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail hidden" id="item3">
+             <img src="/images/panorama/衣服.jpg" data-src="/images/panorama/衣服.jpg" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail item hidden" id="item3">
         </div>
     </div>
     <div class="col-md-12" style="padding-top: 10px">
 
         <div class="text-center col-md-4">
-             <img src="/images/panorama/oneitem.png" data-src="/images/panorama/onepic.png" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail hidden" id="item4">
+             <img src="/images/panorama/oneitem.png" data-src="/images/panorama/onepic.png" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail item hidden" id="item4">
         </div> 
         <div class="text-center col-md-4">
-             <img src="/images/panorama/twoitem.png" data-src="/images/panorama/twopic.png" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail hidden" id="item5">
+             <img src="/images/panorama/twoitem.png" data-src="/images/panorama/twopic.png" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail item hidden" id="item5">
         </div> 
         <div class="text-center col-md-4">
-             <img src="/images/panorama/threeitem.png" data-src="/images/panorama/threepic.png" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail hidden" id="item6">
+             <img src="/images/panorama/threeitem.png" data-src="/images/panorama/threepic.png" data-magnify="gallery" style="height: 300px"  class="img-responsive img-thumbnail item hidden" id="item6">
         </div> 
-    </div>
-    <div class="col-md-12">
-        <button class="btn btn-success item-btn" value="1">青铜鼎</button>
-        <button class="btn btn-success item-btn" value="2">陶罐</button>
-        <button class="btn btn-success item-btn" value="3">古代衣服</button>
-        <button class="btn btn-success item-btn" value="4">像素花纹1</button>
-        <button class="btn btn-success item-btn" value="5">像素花纹2</button>
-        <button class="btn btn-success item-btn" value="6">像素花纹3</button>
     </div>
 </div>
          
@@ -68,9 +60,9 @@ $(document).ready(function() {
 'actualSize',
 'rotateRight']});
 
-    $(".item-btn").on("click", function(e) {
+    $("#person").on("click", function(e) {
         $("#person").addClass("hidden");
-        $("#item" + $(this).val()).removeClass("hidden");
+        $(".item").removeClass("hidden");
     });
 });
 </script>
